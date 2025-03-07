dac7cdebef6c0c7c5bd49ee8255ff5662b505a3f
glamboyosa <ogbemudiatimothy@gmail.com>
2025-03-07 09:54:07 +0100

    feat: update examples dir

--- examples/go.mod
+++ examples/go.mod
@@ -3,12 +3,16 @@ module github.com/glamboyosa/swig/examples
 go 1.23.2
 
 require (
+	github.com/glamboyosa/swig v0.1.3-alpha
+	github.com/jackc/pgx/v5 v5.7.2
+	github.com/lib/pq v1.10.9
+)
+
+require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.7.2 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/crypto v0.35.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 )
--- examples/sql/main.go
+++ examples/sql/main.go
@@ -1,98 +1,99 @@
 package main
 
-// import (
-// 	"context"
-// 	"database/sql"
-// 	"fmt"
-// 	"log"
-// 	"time"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
 
-// 	swig "github.com/glamboyosa/swig"
-// 	"github.com/glamboyosa/swig/drivers"
-// 	_ "github.com/lib/pq"
-// )
+	 "github.com/glamboyosa/swig"
+	"github.com/glamboyosa/swig/drivers"
+	"github.com/glamboyosa/swig/workers"
+	_ "github.com/lib/pq"
+)
 
-// // EmailWorker demonstrates a basic worker implementation
-// type EmailWorker struct {
-// 	To      string `json:"to"`
-// 	Subject string `json:"subject"`
-// 	Body    string `json:"body"`
-// }
+// EmailWorker demonstrates a basic worker implementation
+type EmailWorker struct {
+	To      string `json:"to"`
+	Subject string `json:"subject"`
+	Body    string `json:"body"`
+}
 
-// func (w *EmailWorker) JobName() string {
-// 	return "send_email"
-// }
+func (w *EmailWorker) JobName() string {
+	return "send_email"
+}
 
-// func (w *EmailWorker) Process(ctx context.Context) error {
-// 	fmt.Printf("Sending email to: %s with subject: %s\n", w.To, w.Subject)
-// 	return nil
-// }
+func (w *EmailWorker) Process(ctx context.Context) error {
+	fmt.Printf("Sending email to: %s with subject: %s\n", w.To, w.Subject)
+	return nil
+}
 
-// func main() {
-// 	ctx := context.Background()
+func main() {
+	ctx := context.Background()
 
-// 	// Connect to PostgreSQL using database/sql
-// 	db, err := sql.Open("postgres", "postgres://postgres:postgres@localhost:5432/swig_example?sslmode=disable")
-// 	if err != nil {
-// 		log.Fatalf("Unable to connect to database: %v", err)
-// 	}
-// 	defer db.Close()
+	// Connect to PostgreSQL using database/sql
+	db, err := sql.Open("postgres", "postgres://postgres:postgres@localhost:5432/swig_example?sslmode=disable")
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+	defer db.Close()
 
-// 	// Create SQL driver
-// 	driver, err := drivers.NewSQLDriver(db)
-// 	if err != nil {
-// 		log.Fatalf("Unable to create driver: %v", err)
-// 	}
+	// Create SQL driver
+	driver, err := drivers.NewSQLDriver(db)
+	if err != nil {
+		log.Fatalf("Unable to create driver: %v", err)
+	}
 
-// 	// Create and register workers
-// 	workers := swig.NewWorkerRegistry()
-// 	workers.RegisterWorker(&EmailWorker{})
+	// Create and register workers
+	workers := workers.NewWorkerRegistry()
+	workers.RegisterWorker(&EmailWorker{})
 
-// 	// Configure queues
-// 	configs := []swig.SwigQueueConfig{
-// 		{QueueType: swig.Default, MaxWorkers: 5},
-// 		{QueueType: swig.Priority, MaxWorkers: 2},
-// 	}
+	// Configure queues
+	configs := []swig.SwigQueueConfig{
+		{QueueType: swig.Default, MaxWorkers: 5},
+		{QueueType: swig.Priority, MaxWorkers: 2},
+	}
 
-// 	// Create and start Swig
-// 	swigClient := swig.NewSwig(driver, configs, workers)
-// 	swigClient.Start(ctx)
+	// Create and start Swig
+	swigClient := swig.NewSwig(driver, configs, *workers)
+	swigClient.Start(ctx)
 
-// 	// Add some example jobs
-// 	err = swigClient.AddJob(ctx, &EmailWorker{
-// 		To:      "user@example.com",
-// 		Subject: "Welcome to Swig!",
-// 		Body:    "This is a regular priority job",
-// 	})
-// 	if err != nil {
-// 		log.Printf("Failed to add regular job: %v", err)
-// 	}
+	// Add some example jobs
+	err = swigClient.AddJob(ctx, &EmailWorker{
+		To:      "user@example.com",
+		Subject: "Welcome to Swig!",
+		Body:    "This is a regular priority job",
+	})
+	if err != nil {
+		log.Printf("Failed to add regular job: %v", err)
+	}
 
-// 	// Add a high priority job
-// 	err = swigClient.AddJob(ctx, &EmailWorker{
-// 		To:      "urgent@example.com",
-// 		Subject: "Urgent Notice!",
-// 		Body:    "This is a high priority job",
-// 	}, swig.JobOptions{
-// 		Queue:    swig.Priority,
-// 		Priority: 5,
-// 	})
-// 	if err != nil {
-// 		log.Printf("Failed to add priority job: %v", err)
-// 	}
+	// Add a high priority job
+	err = swigClient.AddJob(ctx, &EmailWorker{
+		To:      "urgent@example.com",
+		Subject: "Urgent Notice!",
+		Body:    "This is a high priority job",
+	}, swig.JobOptions{
+		Queue:    swig.Priority,
+		Priority: 5,
+	})
+	if err != nil {
+		log.Printf("Failed to add priority job: %v", err)
+	}
 
-// 	// Add a scheduled job
-// 	err = swigClient.AddJob(ctx, &EmailWorker{
-// 		To:      "scheduled@example.com",
-// 		Subject: "Scheduled Email",
-// 		Body:    "This job will run in 1 minute",
-// 	}, swig.JobOptions{
-// 		RunAt: time.Now().Add(time.Minute),
-// 	})
-// 	if err != nil {
-// 		log.Printf("Failed to add scheduled job: %v", err)
-// 	}
+	// Add a scheduled job
+	err = swigClient.AddJob(ctx, &EmailWorker{
+		To:      "scheduled@example.com",
+		Subject: "Scheduled Email",
+		Body:    "This job will run in 1 minute",
+	}, swig.JobOptions{
+		RunAt: time.Now().Add(time.Minute),
+	})
+	if err != nil {
+		log.Printf("Failed to add scheduled job: %v", err)
+	}
 
-// 	// Keep the program running
-// 	select {}
-// }
+	// Keep the program running
+	select {}
+}
